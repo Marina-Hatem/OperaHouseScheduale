bc5b7e8d0df2a628993ad9c36ec0aa22c3ee2476
Meriam12 <52137958+Meriam12@users.noreply.github.com>
2020-05-12 11:42:10 +0200

    Toumie classes

---
+++ OperaHouseScheduale/src/operahousescheduale/Class.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Toumie
+ */
+public class Class {
+
+    private final int classId;
+    private final int groupId;
+    private final int CourseId;
+    private int InstructorId;
+    private int timeslotId;
+    private int HallId;
+
+    public Class(int classId, int groupId, int CourseId) {
+        this.classId = classId;
+        this.CourseId = CourseId;
+        this.groupId = groupId;
+    }
+
+    public void addProfessor(int professorId) {
+        this.InstructorId = InstructorId;
+    }
+
+    public void addTimeslot(int timeslotId) {
+        this.timeslotId = timeslotId;
+    }
+
+    public void setRoomId(int roomId) {
+        this.HallId = HallId;
+    }
+
+    public int getClassId() {
+        return this.classId;
+    }
+
+    public int getGroupId() {
+        return this.groupId;
+    }
+
+    public int getCourseId() {
+        return this.CourseId;
+    }
+
+    public int getProfessorId() {
+        return this.InstructorId;
+    }
+
+    public int getTimeslotId() {
+        return this.timeslotId;
+    }
+
+    public int getRoomId() {
+        return this.HallId;
+    }
+}
---
+++ OperaHouseScheduale/src/operahousescheduale/Group.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Toumie
+ */
+public class Group {
+
+    private final int groupId;
+    private final int groupSize;
+    private final int CourseIds[];
+
+    public Group(int groupId, int groupSize, int CourseIds[]) {
+        this.groupId = groupId;
+        this.groupSize = groupSize;
+        this.CourseIds = CourseIds;
+    }
+
+    public int getGroupId() {
+        return this.groupId;
+    }
+
+    public int getGroupSize() {
+        return this.groupSize;
+    }
+
+    public int[] getCourseIds() {
+        return this.CourseIds;
+    }
+}
