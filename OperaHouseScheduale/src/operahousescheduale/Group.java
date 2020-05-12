0b24928c86bedc1107f6e81a4f875fa614d10988
Meriam12 <52137958+Meriam12@users.noreply.github.com>
2020-05-13 00:57:58 +0200

    Group

--- OperaHouseScheduale/src/operahousescheduale/Group.java
+++ OperaHouseScheduale/src/operahousescheduale/Group.java
@@ -8,19 +8,23 @@ package operahousescheduale;
  *
  * @author Toumie
  */
+
+// This class for the trainers that will have courses. It have attributes like group id and group size and array of int which is courseids. 
+//For example group A3 , thery are 20 people and they take ballet course.
 public class Group {
 
     private  int groupId;
     private  int groupSize;
     private  int CourseIds[];
 
+    // Overloaded constractor to inilize the variables 
     public Group(int groupId, int groupSize, int[] CourseIds) {
         this.groupId = groupId;
         this.groupSize = groupSize;
         this.CourseIds = CourseIds;
     }
     
-    
+   // The getters and the setters 
 
     public int getGroupId() {
         return groupId;
