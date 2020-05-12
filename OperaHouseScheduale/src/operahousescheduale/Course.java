207b475c00cfeb804d36e76217e7a93bd94323af
Meriam12 <52137958+Meriam12@users.noreply.github.com>
2020-05-13 00:52:58 +0200

    course

--- OperaHouseScheduale/src/operahousescheduale/Course.java
+++ OperaHouseScheduale/src/operahousescheduale/Course.java
@@ -9,14 +9,23 @@ package operahousescheduale;
  *
  * @author Yasmine
  */
+
+//This class is for the courses that will be taught in the opera house llike ballet, piano , vocals and drawing.
+
 public class Course {
+    // The couse id 
     private int CourseID;
     private String CourseCode;
+    // The course name 
     private String Course;
+    // Array of the ids of the instractors that are in the opera house
     private int InstructorIDS[];
 
+    // The default constractor
     public Course() {
     }
+    
+    // Overloaded constractor to inilizae all the variables.
 
     public Course(int CourseID, String CourseCode, String Course, int[] InstructorIDS) {
         this.CourseID = CourseID;
@@ -25,6 +34,7 @@ public class Course {
         this.InstructorIDS = InstructorIDS;
     }
 
+    //The getters and the setters
     public int getCourseID() {
         return CourseID;
     }
@@ -56,8 +66,13 @@ public class Course {
     public void setInstructorIDS(int[] InstructorIDS) {
         this.InstructorIDS = InstructorIDS;
     }
+    
+    // This function get a random instructor id 
     public int getRandomInstructorID(){
     
+        // There is a variable called Instructorid which stores the random ID
+        // The function Math.random () gets any number by random then it multiplies it by the array length  as the function Math.radnom get number between 0 and 1 so ti get the id 
+        // inisde the array it multiply by the length and it cast it into array of intgers
         int InstructorId=InstructorIDS[(int)(InstructorIDS.length * Math.random())];
         return InstructorId;
     }
