cd8a0648c16dd05be995fb2f914de77ed791ec3b
Meriam12 <52137958+Meriam12@users.noreply.github.com>
2020-05-12 12:12:44 +0200

    hall

--- OperaHouseScheduale/src/operahousescheduale/Hall.java
+++ OperaHouseScheduale/src/operahousescheduale/Hall.java
@@ -9,21 +9,28 @@ package operahousescheduale;
  *
  * @author meriam
  */
+
+// This class for the halls that are avaliable in the opera house that the instructors will teach in it the courses.
+
 public class Hall {
     
     private int hallID;
+    // Hall capcity to know how many trainers can be in the hall , and to know the big and small halls.
     private int capacity;
+    // Hall number like hal12 or hall6.
     private String hallNumber;
 
     public Hall() {
     }
 
+    // Constractor for the class to initatite in it the variabkes.
     public Hall(int hallID, int capacity, String hallNumber) {
         this.hallID = hallID;
         this.capacity = capacity;
         this.hallNumber = hallNumber;
     }
 
+    // The getters and the setters of the variables.
     public int getHallID() {
         return hallID;
     }
